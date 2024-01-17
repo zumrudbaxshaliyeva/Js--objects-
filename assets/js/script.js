f82d3bb513204eaaf11397c9ed89773d4907f85d
zumrudbaxshaliyeva <zumrudbaxsaliyeva@gmail.com>
2024-01-17 16:31:44 +0400

    hometasks solved

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+
+
+/*---------------------TASK1--------------------*/
+
+// const portfolio={
+//     name:"Zumrud",
+//     surname:"Bakhshaliyeva",
+//     age:22,
+//     universityInfo:{
+//     university:"ASOIU",
+//     profession:"computer engineering"
+//     }
+// }
+// console.log(portfolio)
+// console.log(portfolio.universityInfo)
+// console.log(portfolio.name)
+// console.log(portfolio.universityInfo["profession"])
+
+
+/*---------------------TASK2--------------------*/
+
+// const personalPortfolio={
+//     firstName:"Zumrud",
+//     lastName:"Bakhshaliyeva",
+//     age:22,
+//     favoriteLanguage:"JavaScript",
+//     contactInfo:{
+//         email:"zumrudbaxsaliyeva@gmail.com",
+//         phone:"+994506123749"
+//     }
+// }
+
+
+/*---------------------TASK3--------------------*/
+
+// const personalPortfolio={
+//     firstName:"Zumrud",
+//     lastName:"Bakhshaliyeva",
+//     age:22,
+//     favoriteLanguage:"JavaScript",
+//     contactInfo:{
+//         email:"zumrudbaxsaliyeva@gmail.com",
+//         phone:"+994506123749"
+//     }
+// }
+
+// personalPortfolio["hobby"]="Listen to music"
+// console.log(personalPortfolio)
+// console.log(personalPortfolio.hobby)
+
+/*---------------------TASK4--------------------*/
+
+const personalPortfolio={
+    firstName:"Zumrud",
+    lastName:"Bakhshaliyeva",
+    age:22,
+    favoriteLanguage:"JavaScript",
+    contactInfo:{
+        email:"zumrudbaxsaliyeva@gmail.com",
+        phone:"+994506123749"
+    }
+}
+console.log(personalPortfolio.age)
+console.log(personalPortfolio["contactInfo"]["email"])
+console.log(personalPortfolio["contactInfo"].phone)
